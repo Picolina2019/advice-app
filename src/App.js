9e392081f336e1a34104d4dc7f7db4ba4edc4b1c
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-05-06 17:35:29 +0200

    Update App.js
    UI corrections

--- src/App.js
+++ src/App.js
@@ -5,31 +5,42 @@ import axios from 'axios';
 
 class App extends Component {
   state={
-    advice:''
+    advice:'',
+    isLoading:false,
+    error:false
   };
   componentDidMount (){
     this.fetchAdvice();
   }
   fetchAdvice=()=>{
+    this.setState({isLoading:true})
     axios.get('https://api.adviceslip.com/advice')
     .then((response)=>{
       const{ advice}= response.data.slip
-      this.setState({ advice:advice});
+      this.setState({ advice:advice, isLoading:false});
+
     })
     .catch((error)=>{
-      console.log(error);
+      this.setState({ advice:null, isLoading:false, error:true});
     });
   }
   render(){
    return (
+     
+   
   <div className="app">
-    <div className='card'>
+  {this.state.error ? (<h1 style={{color:'red', fontSize:'7rem'}}>Something went wrong</h1>) :
+   (<div className='card'>
+      {this.state.isLoading ? (<p>Loading...</p>):
+      (<>
       <h2 className='heading'>{this.state.advice}</h2>
       <button onClick={this.fetchAdvice} className='button'>
         <span>Give me advice</span>
-      </button>
-    </div>
+       
+      </button> </>)}
+    </div>)}
    </div>
+   
   );
 }
 }
